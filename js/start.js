01822740199adcbd9c0450eaca95d33b817c4b7e
ruinak <teezeei@naver.com>
2021-09-11 18:11:07 +0900

    Result Page
    Result 구현, 결과 계산하기, Result 디자인

--- js/start.js
+++ js/start.js
@@ -3,6 +3,7 @@ const main = document.querySelector('#main');
 const qna = document.querySelector('#qna');
 const result = document.querySelector('#result');
 const endPoint = 12;
+const select = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 function begin() {
   main.style.WebkitAnimation = 'fadeOut 1s';
@@ -21,18 +22,23 @@ function begin() {
 }
 
 function goNext(qIdx) {
+  if (qIdx === endPoint) {
+    goResult();
+    return;
+  }
   var q = document.querySelector('.qBox');
   // data.js에서 q를 꺼내옴
   q.innerHTML = qnaList[qIdx].q;
   // data.js에서 answer를 꺼내옴
   for (let i in qnaList[qIdx].a) {
-    addAnswer(qnaList[qIdx].a[i].answer, qIdx);
+    addAnswer(qnaList[qIdx].a[i].answer, qIdx, i);
   }
   var status = document.querySelector('.statusBar');
   status.style.width = (100 / endPoint) * (qIdx + 1) + '%';
+  // 마지막 페이지에 도달하면 Result 호출
 }
 
-function addAnswer(answerText, qIdx) {
+function addAnswer(answerText, qIdx, idx) {
   var a = document.querySelector('.answerBox');
   // 버튼을 answer 변수에 담음
   var answer = document.createElement('button');
@@ -56,6 +62,11 @@ function addAnswer(answerText, qIdx) {
         children[i].style.animation = 'fadeOut 0.5s';
       }
       setTimeout(() => {
+        var target = qnaList[qIdx].a[idx].type;
+        for (let i = 0; i < target.length; i++) {
+          select[target[i]] += 1;
+        }
+
         for (let i = 0; i < children.length; i++) {
           children[i].style.display = 'none';
         }
@@ -64,4 +75,42 @@ function addAnswer(answerText, qIdx) {
     },
     false
   );
+}
+
+function goResult() {
+  qna.style.WebkitAnimation = 'fadeOut 1s';
+  qna.style.animation = 'fadeOut 1s';
+  setTimeout(() => {
+    result.style.WebkitAnimation = 'fadeIn 1s';
+    result.style.animation = 'fadeIn 1s';
+    setTimeout(() => {
+      qna.style.display = 'none';
+      result.style.display = 'block';
+    }, 450);
+  });
+  setResult();
+  calResult();
+}
+
+function setResult() {
+  let point = calResult();
+  const resultName = document.querySelector('.resultName');
+  resultName.innerHTML = infoList[point].name;
+
+  var resultImg = document.createElement('img');
+  const imgDiv = document.querySelector('#resultImg');
+  var imgURL = 'img/image-' + point + '.png';
+  resultImg.src = imgURL;
+  resultImg.alt = point;
+  resultImg.classList.add('img-fluid');
+  imgDiv.appendChild(resultImg);
+
+  const resultDesc = document.querySelector('.resultDesc');
+  resultDesc.innerHTML = infoList[point].desc;
+}
+
+function calResult() {
+  // 전개연산자 사용
+  var result = select.indexOf(Math.max(...select));
+  return result;
 }
